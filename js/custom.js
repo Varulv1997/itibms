0c41b131ce3cb2f35704edda60e4b38293665590
varulv1997 <farahianas5@gmail.com>
2020-09-04 15:34:14 +0100

    -rw1

--- js/custom.js
+++ js/custom.js
@@ -535,4 +535,4 @@ POTENZA.pieChart = function () {
 document.documentElement.className="js";var supportsCssVars=function(){
   var e,t=document.createElement("style");
   return t.innerHTML="root: { --tmp-var: bold; }",document.head.appendChild(t),e=!!(window.CSS&&window.CSS.supports&&window.CSS.supports("font-weight","var(--tmp-var)")),t.parentNode.removeChild(t),e};
-  supportsCssVars()||alert("Please view this demo in a modern browser that supports CSS Variables.");
+  supportsCssVars()||alert("Please view this website in a modern browser that supports CSS Variables.");
